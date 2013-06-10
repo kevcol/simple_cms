43da994a2148a32fd07fe347fb7d92a68a39795a
kevcol <kevincolligan@gmail.com>
2013-06-09 20:20:22 -0700

    Sending up the test files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = 'adfddff7b8b76697d6f9543c603607cc0a8df0d032d10b0712725a51ddbc9e838af2bf35f130df1dcaeb259431f6304990ded2b89be20948c8908c6eb068e8a1'
